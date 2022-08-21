5ee30d14137f10c488331a19eaea42e7c83f7fe1
BlackDes <77753565+BlackDes@users.noreply.github.com>
2022-08-21 04:28:06 +0300

    Добавьте файлы проекта.

---
+++ 21.12.01.2/Grafiki.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace _21._12._01._2
+{
+	/// <summary>
+	/// Логика взаимодействия для Grafiki.xaml
+	/// </summary>
+	public partial class Grafiki : Window
+	{
+		public Grafiki()
+		{
+			InitializeComponent();
+
+			tbxXminBogidaev.Text = "-1,5";
+			tbxXmaxBogidaev.Text = "-2,5";
+			tbxStepBogidaev.Text = "-0,5";
+
+			tbxXminDolgikh.Text = "-4";
+			tbxXmaxDolgikh.Text = "-6,2";
+			tbxStepDolgikh.Text = "-0,2";
+		}
+
+		private void BtnOutput_Click(object sender, RoutedEventArgs e)
+		{
+			double Xmin = 0, Xmax = 0, Step = 0, XminD = 0, XmaxD = 0, StepD = 0;
+
+			if ((!double.TryParse(tbxXminBogidaev.Text, out Xmin)) || (!double.TryParse(tbxXmaxBogidaev.Text, out Xmax)) || (!double.TryParse(tbxStepBogidaev.Text, out Step)) ||
+				(!double.TryParse(tbxXminDolgikh.Text, out XminD)) || (!double.TryParse(tbxXmaxDolgikh.Text, out XmaxD)) || (!double.TryParse(tbxStepDolgikh.Text, out StepD)))
+			{
+				MessageBox.Show("Введите числа! Дробные числа вводятся с помощью запятой!", "Ошибка при ввводе");
+			}
+			else
+			{
+				if ((((Step < 0) && (Xmax > Xmin)) || ((Step > 0) && (Xmax < Xmin))) || (Math.Abs(Step) > Math.Abs(Xmax - Xmin)) ||
+					(((StepD < 0) && (XmaxD > XminD)) || ((StepD > 0) && (XmaxD < XminD))) || (Math.Abs(StepD) > Math.Abs(XmaxD - XminD)))
+				{
+					MessageBox.Show("Введите корректные значения.\n1)Если шаг больше нуля, то Xmax должен быть больше Xmin.\n2)Если же шаг меньше нуля, то Xmax должен быть меньше Xmin.\n" +
+										"3)Шаг не должен превышать разность иксов");
+				}
+				else
+				{
+					int count = Convert.ToInt32((Xmax - Xmin) / Step + 1);
+
+					double[] x = new double[count];
+					double[] y = new double[count];
+
+					for (int i = 0; i < count; i++)
+					{
+						x[i] = Xmin + Step * i;
+						y[i] = Math.Pow(x[i], 2) + Math.Tan(5 * x[i] - 0.8 / x[i]);
+					}
+
+
+
+					int countD = Convert.ToInt32((XmaxD - XminD) / StepD + 1);
+
+					double[] xD = new double[countD];
+					double[] yD = new double[countD];
+
+					for (int i = 0; i < countD; i++)
+					{
+						xD[i] = XminD + StepD * i;
+						yD[i] = xD[i] + Math.Sqrt(Math.Abs(Math.Pow(xD[i], 3) + 0.1 - Math.Exp(xD[i])));
+					}
+
+
+
+					chart.ChartAreas.Clear();
+					chart.ChartAreas.Add(new ChartArea("ChartArea"));
+
+					chart.Series.Clear();
+
+					chart.Series.Add(new Series("Series1"));
+					chart.Series["Series1"].ChartArea = "ChartArea";
+					chart.Series["Series1"].ChartType = SeriesChartType.Line;
+					chart.Series["Series1"].Points.DataBindXY(x, y);
+
+					chart.Series.Add(new Series("Series2"));
+					chart.Series["Series2"].ChartArea = "ChartArea";
+					chart.Series["Series2"].ChartType = SeriesChartType.Line;
+					chart.Series["Series2"].Points.DataBindXY(xD, yD);
+				}
+			}
+		}
+	}
+}
---
+++ 21.12.01.2/ShtrikhKodi.xaml.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+
+namespace _21._12._01._2
+{
+	public partial class ShtrikhKodi : Window
+	{
+		Dictionaries d = new Dictionaries();
+        Random rand = new Random();
+
+        int[] numbers = new int[13];
+
+        string str = "101";
+		string str7;
+
+        public ShtrikhKodi()
+		{
+			InitializeComponent();
+
+			GenerarionShtrikhKodi();
+		}
+
+		public void GenerarionShtrikhKodi()
+        {
+            tbxChisla.Text = null;
+            lblDvoichnie.Content = null;
+
+			byte set = 0;
+			int chet = 0, nechet = 0, x = 40, y = 10, height = 150, widht = 4;
+
+			for (int i = 0; i < 12; i++)
+			{
+				numbers[i] = rand.Next(0, 10);
+                tbxChisla.Text += numbers[i].ToString();
+
+                if ((i + 1) % 2 == 0)
+				{
+					chet += numbers[i];
+				}
+
+				if ((i + 1) % 2 == 1)
+				{
+					nechet += numbers[i];
+				}
+
+				if (i == 0 || i == 6)
+				{
+					tbxChisla.Text += "    ";
+                }
+            }
+
+            numbers[12] = ((chet * 3 + nechet) / 10 + 1) * 10 - (chet * 3 + nechet);
+
+			if (numbers[12] == 10)
+            {
+				numbers[12] = 0;
+            }
+
+            tbxChisla.Text += numbers[12].ToString();
+
+			int a;
+
+            for (int i = 1; i < 7; i++)
+            {
+                a = numbers[i];
+
+                set = d.RightPart[numbers[0], i - 1];
+
+                str7 = d.Sets[numbers[i], set];
+
+                str += str7;
+
+                lblDvoichnie.Content += $" {str7}";
+            }
+
+			lblDvoichnie.Content += "\n";
+
+			str += "01010";
+
+            for (int i = 7; i < 13; i++)
+            {
+				a = numbers[i];
+
+				str7 = d.Sets[numbers[i], 2];
+
+                str += str7;
+
+                lblDvoichnie.Content += $" {str7}";
+            }
+
+			str += "101";
+
+			char[] ch = str.ToCharArray();
+
+			for (int i = 0; i < 95; i++)
+			{
+				Line line = new Line();
+
+				line.Stroke = Brushes.Black;
+				line.StrokeThickness = widht;
+				line.X1 = x;
+                line.X2 = x;
+
+                if (i == 0 || i == 2 || i == 46 || i == 48 || i == 92 || i == 94)
+                {
+                    line.Y1 = y;
+					line.Y2 = y + height + height / 5;
+                }
+				else
+                {
+					line.Y1 = y;
+					line.Y2 = y + height;
+				}
+
+				if (ch[i] == '1')
+				{
+					canvas.Children.Add(line);
+				}
+
+				x += widht;
+			}
+		}
+
+        private void btnPeregenirovanie_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(Application.ResourceAssembly.Location);
+			Application.Current.Shutdown();
+		}
+    }
+}
